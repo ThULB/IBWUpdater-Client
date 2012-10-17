48900c24af793a13cb9d7d49f1d2f1534416bd80
adlerre <adlerre@8739d80e-6a82-4124-b353-9488188f9498>
2012-10-17 05:40:41 +0000

    initial commit of IBWUpdater client

---
+++ trunk/client/chrome/ibw/content/xul/IBWUpdaterDialog.js
@@ -0,0 +1,68 @@
+/**
+ * @version 1.0 - $Revision$ (beta)
+ * @author René Adler
+ * 
+ * This program is free software; you can use it, redistribute it and / or
+ * modify it under the terms of the GNU General Public License (GPL) as
+ * published by the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program, in a file called gpl.txt or license.txt. If not, write to the
+ * Free Software Foundation Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307 USA
+ */
+
+var updater;
+var wasPrevStarted = false;
+
+function onLoad() {
+	try {
+		//updater = window.arguments[0].wrappedJSObject;
+		updater = new IBWUpdater();
+	} catch (ex) {
+		application.messageBox("IBWUpdater", ex.toString(), "error-icon");
+	}
+}
+
+function onClose() {
+	try {
+		return !updater.isProcessing();
+	} catch (ex) {
+		return true;
+	}
+	
+}
+
+function onFocus() {
+	try {
+		if (!updater.isProcessing() && !wasPrevStarted) {
+			wasPrevStarted = true;
+			startProcessing();
+		}
+	} catch (ex) {
+		application.messageBox("IBWUpdater", ex.toString(), "error-icon");
+	}
+}
+
+function startProcessing() {
+	try {
+		updater.processPackages(function(progress) {
+			document.getElementById("lbPackageName").label = I18N.getLocalizedMessage("package.process", progress.name);
+			document.getElementById("lbStep").value = progress.step;
+
+			document.getElementById("pbSingle").value = progress.single;
+			document.getElementById("lbProgressSingle").label = progress.single + "%";
+
+			document.getElementById("pbTotal").value = progress.total;
+			document.getElementById("lbProgressTotal").label = progress.total + "%";
+		});
+	} catch (ex) {
+		application.messageBox("IBWUpdater", ex.toString(), "error-icon");
+	}
+}
