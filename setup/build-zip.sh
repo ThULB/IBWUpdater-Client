efbeb1747d06e6bc3b77c5558f985c57bf331ccd
adlerre <rene.adler@electric-force.net>
2013-05-08 06:09:39 +0000

    fix path for zip entries

--- setup/build-zip.sh
+++ setup/build-zip.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -rf ibwupd.zip
-zip -r --exclude=*.svn* --exclude=*.DS_Store* --exclude=*setup* --exclude=*.sh ./ibwupd.zip ../*
+cd ..
+zip -r --exclude=*.svn* --exclude=*.DS_Store* --exclude=*setup* --exclude=*.sh ./setup/ibwupd.zip *
+cd setup
