fe3ce375b7928941fb71167ec5c78cb093878801
René Adler <rene.adler@uni-jena.de>
2017-04-24 09:43:01 +0200

    prepare next release

--- GruntFile.js
+++ GruntFile.js
@@ -1,13 +1,17 @@
+String.prototype.endsWith = function(suffix) {
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
 module.exports = function(grunt) {
 	var path = require("path");
 	var fs = require("fs");
 
 	// Project configuration.
 	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'),
-		banner : '/*!\n' + ' * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.revision %>)\n' + ' * Homepage: <%= pkg.homepage %>\n'
-				+ ' * (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %> and others. All rights reserved.\n'
-				+ ' * Licensed under <%= pkg.license %>\n' + ' */\n',
+		pkg : grunt.file.readJSON("package.json"),
+		banner : "/*!\n" + " * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.revision %>)\n" + " * Homepage: <%= pkg.homepage %>\n"
+				+ " * (c) 2013-<%= grunt.template.today(\"yyyy\") %> <%= pkg.author %> and others. All rights reserved.\n"
+				+ " * Licensed under <%= pkg.license %>\n" + " */\n",
 		clean : {
 			build : {
 				src : [ "build", "dist" ]
@@ -16,31 +20,31 @@ module.exports = function(grunt) {
 		uglify : {
 			build : {
 				options : {
-					banner : '<%= banner %>',
-					preserveComments : 'some',
+					banner : "<%= banner %>",
+					preserveComments : "some",
 					sourceMap : false,
 					screwIE8 : true
 				},
 				files : [ {
 					expand : true,
-					cwd : 'resources/',
-					src : '**/*.js',
-					dest : 'build/<%= pkg.name %>/'
+					cwd : "resources/",
+					src : "**/*.js",
+					dest : "build/<%= pkg.name %>/"
 				} ]
 			}
 		},
 		copy : {
 			build : {
 				expand : true,
-				cwd : 'resources/',
-				src : '**',
-				dest : 'build/<%= pkg.name %>'
+				cwd : "resources/",
+				src : "**",
+				dest : "build/<%= pkg.name %>"
 			},
 			setup : {
 				expand : true,
-				cwd : 'setup/',
-				src : '**',
-				dest : 'build/setup'
+				cwd : "setup/",
+				src : "**",
+				dest : "build/setup"
 			}
 		},
 		gitinfo : {
@@ -52,79 +56,79 @@ module.exports = function(grunt) {
 			build : {
 				options : {
 					patterns : [ {
-						match : 'Version',
-						replacement : '<%= pkg.version %>'
+						match : "Version",
+						replacement : "<%= pkg.version %>"
 					}, {
-						match : 'Revision',
-						replacement : '<%= gitinfo.local.branch.current.shortSHA %>'
+						match : "Revision",
+						replacement : "<%= gitinfo.local.branch.current.shortSHA %>"
 					} ]
 				},
 				files : [ {
 					expand : true,
 					flatten : false,
-					cwd : 'build/',
-					src : [ '**' ],
-					dest : 'build/'
+					cwd : "build/",
+					src : [ "**" ],
+					dest : "build/"
 				} ]
 			}
 		},
 		compress : {
 			dist : {
 				options : {
-					archive : 'dist/<%= pkg.name %>.zip'
+					archive : "dist/<%= pkg.name %>.zip"
 				},
 				files : [ {
 					expand : true,
-					cwd : 'build/<%= pkg.name %>/chrome',
-					src : [ 'content/**/*', 'lib/**/*', 'locale/**/*' ],
-					dest : 'chrome/ibw'
+					cwd : "build/<%= pkg.name %>/chrome",
+					src : [ "content/**/*", "lib/**/*", "locale/**/*" ],
+					dest : "chrome/ibw"
 				}, {
 					expand : true,
-					cwd : 'build/<%= pkg.name %>',
-					src : [ 'scripts/*' ],
-					dest : ''
+					cwd : "build/<%= pkg.name %>",
+					src : [ "scripts/*" ],
+					dest : ""
 				} ]
 			}
 		},
 		curl : {
 			innounp : {
-				src : 'https://downloads.sourceforge.net/project/innounp/innounp/innounp%200.45/innounp045.rar?r=&ts=1439566551&use_mirror=skylineservers',
-				dest : 'build/setup/innosetup/innounp.rar'
+				src : "https://downloads.sourceforge.net/project/innounp/innounp/innounp%200.45/innounp045.rar?r=&ts=1439566551&use_mirror=skylineservers",
+				dest : "build/setup/innosetup/innounp.rar"
 			},
 			innosetup : {
-				src : 'http://files.jrsoftware.org/is/5/isetup-5.5.8-unicode.exe',
-				dest : 'build/setup/innosetup/is-unicode.exe'
+				src : "http://files.jrsoftware.org/is/5/isetup-5.5.8-unicode.exe",
+				dest : "build/setup/innosetup/is-unicode.exe"
 			}
 		},
 		exec : {
 			innounp : {
-				command : 'unrar -y e innounp.rar',
-				cwd : 'build/setup/innosetup/'
+				command : "unrar -y e innounp.rar",
+				cwd : "build/setup/innosetup/"
 			},
 			innosetup : {
-				command : 'wine ./innounp.exe -e ./is-unicode.exe',
-				cwd : 'build/setup/innosetup/'
+				command : "wine ./innounp.exe -e ./is-unicode.exe",
+				cwd : "build/setup/innosetup/"
 			},
 			installer : {
-				command : 'wine ./innosetup/ISCC.exe IBWUpdater.iss /Dsources=..\\\\<%= pkg.name %>\\\\ /O..\\\\..\\\\dist\\\\',
-				cwd : 'build/setup/'
+				command : "wine ./innosetup/ISCC.exe IBWUpdater.iss /Dsources=..\\\\<%= pkg.name %>\\\\ /O..\\\\..\\\\dist\\\\",
+				cwd : "build/setup/"
 			}
 		}
 	});
 
 	// Build Tasks
-	grunt.loadNpmTasks('grunt-curl');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-compress');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks("grunt-curl");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-exec");
 	grunt.loadNpmTasks("grunt-gitinfo");
-	grunt.loadNpmTasks('grunt-replace');
+	grunt.loadNpmTasks("grunt-replace");
 
-	grunt.registerTask('build', [ 'gitinfo', 'copy', 'uglify', 'replace', 'compress' ]);
-	grunt.registerTask('setup', [ 'clean', 'build', 'curl', 'exec' ]);
+	grunt.registerTask("build", [ "gitinfo", "copy", "uglify", "replace", "compress" ]);
+	grunt.registerTask("setup", [ "clean", "build", "curl", "exec" ]);
 
-	grunt.registerTask('default', [ 'clean', 'build' ]);
+	grunt.registerTask("default", [ "clean", "build" ]);
 };
